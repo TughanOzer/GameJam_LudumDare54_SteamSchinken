0860164d45e34e25bb23c2cc0b5bfcbdac3b037d
Tughan <96036066+TughanOzer@users.noreply.github.com>
2023-09-30 17:44:56 +0200

    Update for the Round Manager

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -1,32 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed = 5f;
-    public Transform movePoint;
+    public Transform moveTarget;
 
     public LayerMask barrierObjects;
 
+    public Animator playerAnim;
+
+    public GameRoundManager gameRoundManager;
+
     private void Start() {
-        movePoint.parent = null;
+        moveTarget.parent = null;
+        gameRoundManager.GetComponent<GameRoundManager>();
     }
 
-    private void Update() {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    private void FixedUpdate() {
+        if (!gameRoundManager.ongoingRoundEnemy && gameRoundManager.ongoingRoundPlayer) {
+            transform.position = Vector3.MoveTowards(transform.position, moveTarget.position, moveSpeed * Time.deltaTime);
 
-        if(Vector3.Distance(transform.position, movePoint.position) <= .05f) {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            if (Vector3.Distance(transform.position, moveTarget.position) <= .05f) {
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+
+                    if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, barrierObjects)) {
+                        moveTarget.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    }
+                }
+                else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+
+                    if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, barrierObjects)) {
+                        moveTarget.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    }
+                }
+                //playerAnim.SetBool("moving", false);
+
+                //EnemyTurn
+                gameRoundManager.FinishPlayerTurn();
             }
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+
+            else {
+                //playerAnim.SetBool("moving", true);
+                
             }
         }
-
-
     }
+
+
+
 
 }
