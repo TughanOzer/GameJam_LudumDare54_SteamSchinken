ed50a47a661548f57616e32626f558c8a36a06e7
EndocrineMSc <bamsc.studio@gmail.com>
2023-10-01 15:57:08 +0200

    FMOD Integration

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPosition)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPosition);
+    }
+
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        return eventInstance;
+    }
+}
--- Assets/Scripts/EnergyPickUp.cs
+++ Assets/Scripts/EnergyPickUp.cs
@@ -1,15 +1,34 @@
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnergyPickUp : MonoBehaviour
 {
+    [SerializeField] private EventReference _energyPickUpSound;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log(collision.gameObject.name);
+
         if (collision.gameObject.TryGetComponent<Player>(out var player))
         {
+            Debug.Log("Player Collision!");
             player.AddStealthUses();
-            //todo: play pickup sound;
+            AudioManager.Instance.PlayOneShot(_energyPickUpSound, transform.position);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.TryGetComponent<Player>(out var player))
+        {
+            Debug.Log("Player Collision!");
+            player.AddStealthUses();
+            AudioManager.Instance.PlayOneShot(_energyPickUpSound, transform.position);
             Destroy(gameObject);
         }
     }
