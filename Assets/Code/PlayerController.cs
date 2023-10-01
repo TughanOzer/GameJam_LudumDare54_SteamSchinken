163993f6cd357a46500544751098664a1884af2b
Tughan <96036066+TughanOzer@users.noreply.github.com>
2023-10-01 04:43:25 +0200

    Added new RoundManager with better functionalities

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
     public float moveSpeed = 5f;
     public Transform moveTarget;
@@ -12,45 +12,53 @@ public class PlayerController : MonoBehaviour
     public LayerMask barrierObjects;
 
     public Animator playerAnim;
-
-    public GameRoundManager gameRoundManager;
+    [SerializeField] RoundManager2 roundManager2;
 
     private void Start() {
         moveTarget.parent = null;
-        gameRoundManager.GetComponent<GameRoundManager>();
+        roundManager2 = FindObjectOfType<RoundManager2>();
     }
 
-    private void FixedUpdate() {
-        if (!gameRoundManager.IsOngoingRoundEnemy && gameRoundManager.IsOngoingRoundPlayer) {
-            transform.position = Vector3.MoveTowards(transform.position, moveTarget.position, moveSpeed * Time.deltaTime);
+    public IEnumerator PlayerWalk() {
+        if (roundManager2.state == RoundState.PLAYERTURN) {
+            //playerAnim.SetBool("moving", true);
+            while (Vector3.Distance(transform.position, moveTarget.position) > .000005f) {
 
-            if (Vector3.Distance(transform.position, moveTarget.position) <= .05f) {
-                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+                //Debug.Log("Player currently walking");
+                transform.position = Vector3.Lerp(transform.position, moveTarget.position, moveSpeed * Time.deltaTime);
 
-                    if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, barrierObjects)) {
-                        moveTarget.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                    }
+                if (Vector3.Distance(transform.position, moveTarget.position) <= .000005f) {
+                    //Hier Zeug für Player Runden Ende:
+                    transform.position = moveTarget.position;
+                    //playerAnim.SetBool("moving", false);
+                    roundManager2.state = RoundState.ENEMYTURN;
+                    Debug.Log("Ende wurde ausgeführt.");
+                    roundManager2.EnemyTurn();
                 }
-                else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-
-                    if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, barrierObjects)) {
-                        moveTarget.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                    }
-                }
-                //playerAnim.SetBool("moving", false);
-
-                //EnemyTurn
-                gameRoundManager.FinishPlayerTurn();
+                yield return null;
             }
 
-            else {
-                //playerAnim.SetBool("moving", true);
-                
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+                // Check if colliding horizontally
+                if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, barrierObjects)) {
+                    moveTarget.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
             }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+                // Check if colliding vertically
+                if (!Physics2D.OverlapCircle(moveTarget.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, barrierObjects)) {
+                    moveTarget.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+
+
         }
     }
 
 
+    void ReachedGoal() {
 
+    }
 
 }
