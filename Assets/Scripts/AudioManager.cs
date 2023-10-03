179f4e389a14eb44b6ec1a051ce12b0fa4e130cd
EndocrineMSc <bamsc.studio@gmail.com>
2023-10-03 19:21:43 +0200

    Repaired Audio Sliders for Build

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -19,10 +19,6 @@ public class AudioManager : MonoBehaviour
     [Range(0, 1)] public float MusicVolume = 1;
     [Range(0, 1)] public float SoundVolume = 1;
 
-    private Bus _masterBus;
-    private Bus _soundBus;
-    private Bus _musicBus;
-
     private void Awake()
     {
         if (Instance == null)
@@ -34,17 +30,13 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
-        _masterBus = RuntimeManager.GetBus("bus:/");
-        _musicBus = RuntimeManager.GetBus("bus:/Music");
-        _soundBus = RuntimeManager.GetBus("bus:/SFX");
     }
 
     private void Update()
     {
-        _masterBus.setVolume(MasterVolume);
-        _musicBus.setVolume(MusicVolume);
-        _soundBus.setVolume(SoundVolume);
+        RuntimeManager.GetBus("bus:/").setVolume(MasterVolume);
+        RuntimeManager.GetBus("bus:/Music").setVolume(MusicVolume);
+        RuntimeManager.GetBus("bus:/SFX").setVolume(SoundVolume);
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPosition)
